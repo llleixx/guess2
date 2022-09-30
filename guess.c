add921e12a587e443490a9ca14d36c884a2ed68a
lllei <llleigoing@outlook.com>
2022-09-30 17:02:49 +0800

    fix bug

--- guess.c
+++ guess.c
@@ -1,103 +1,107 @@
 #include <stdio.h>
 #include <string.h>
 #include<time.h>
+#include<stdlib.h>
 
-char id[100],pwd[100],text[100];
-int ok = 0;
+char id[20],pwd[20],text[50];
 
 //登录 
 int login()
 {
-    printf("登录界面\n");
-    printf("ID：");
-    gets(id);
-    printf("PWD：");
-    gets(pwd); 
-    strcat(id,pwd);
-    strcat(id,"\n");
-    
-	int len = 0, ok=0;
-    FILE *fp = fopen("user.txt", "r");
-    if(NULL == fp)
-    {
-        printf("Failed to open user.txt\n");
-        return 0; 
-    }
-
-    while(!feof(fp))
-    {
-        memset(text, 0, sizeof(text));
-        fgets(text, sizeof(text) - 1, fp); 
-        if(strcmp(id,text) == 0)
+	printf("登录界面\n");
+	printf("ID：");
+	gets(id);
+	printf("PWD：");
+	gets(pwd); 
+	strcat(id,pwd);
+	strcat(id,"\n");
+	
+	int ok=0;
+	FILE *fp = fopen("user.txt", "r");
+	if(NULL == fp)
+	{
+		printf("Failed to open user.txt\n");
+		return 0; 
+	}
+	
+	while(!feof(fp))
+	{
+		memset(text, 0, sizeof(text));
+		fgets(text, sizeof(text) - 1, fp); 
+		if(strcmp(id,text) == 0)
 		{
-           ok=1;
-           break;
-        }
-    }
-    
-    if(ok)
-    {
-      printf("登录成功!\n");
+			ok=1;
+			break;
+		}
 	}
-    else
-    {
-      printf("登录失败!\n");
+	
+	if(ok)
+	{
+		printf("登录成功!\n");
 	}
+	else
+	{
+		printf("登录失败!\n");
+		login();
+	}	
 	fclose(fp);
+	return 0;
 }
 
 //注册 
 void regist()
 {
 	printf("注册界面\n");
-    FILE *fp = fopen("user.txt","a");
-    printf("ID：");
-    gets(id);
-    printf("PWD：");
-    gets(pwd); 
-    fputs(id,fp);
-    fputs(pwd,fp);
-    fputs("\n",fp);
-    fflush(stdin);
-    fclose(fp);
-    login();
+	FILE *fp = fopen("user.txt","a");
+	printf("ID：");
+	gets(id);
+	printf("PWD：");
+	gets(pwd); 
+	fputs(id,fp);
+	fputs(pwd,fp);
+	fputs("\n",fp);
+	fflush(stdin);
+	fclose(fp);
+	login();
 }
 
 //游戏 
 void game()
 {	 
+	srand(time(0));
 	clock_t start, end;
-    start = clock();
-    printf("runtime = %f\n",run_time);
-    int num = rand() % 1000;  
-    int guess, i = 0;   
-    while (1)
+	start = clock();
+	int num = rand() % 1000;  
+	int guess, i = 0;   
+	int min = 0, max = 1000; 
+	while (1)
 	{
-    	printf("Please guess the number:");
-        scanf("%d", &guess);
-        if (num > guess)
+		printf("Please guess the number:");
+		scanf("%d", &guess);
+		if (num > guess)
 		{
-            i++;	
-            printf("猜小了\n");
-            min = guess;
-            printf("范围:%d - %d\n", min, max);
-        }
+			i++;	
+			printf("猜小了\n");
+			min = guess;
+			printf("范围:%d - %d\n", min, max);
+		}
 		else if (num < guess)
 		{
-            i++;	
-            printf("猜大了\n");
-            max = guess;
-            printf("范围:%d - %d\n", min, max);
-        }
+			i++;	
+			printf("猜大了\n");
+			max = guess;
+			printf("范围:%d - %d\n", min, max);
+		}
 		else
 		{
-            i++;	
-            end = clock();
-            printf("猜对了!\n猜数次数：%d次\n用时：%d秒", i, t);  
-            break;
-        }
- 
-    }
+			i++;	
+			end = clock();
+			int t=(end-start)/1000;
+			printf("猜对了!\n猜数次数：%d次\n用时：%d秒", i, t);  
+			break;
+		}
+		
+	}
 }
 
 //菜单 
@@ -105,34 +109,38 @@ void meun()
 {
 	printf("1 注册\n");
 	printf("0 登录\n");
-    int a = 0;
-    scanf("%d",&a);
-    fflush(stdin);
-    if(a)
-       regist();
-    else
-        login();
+	int a = 0;
+	scanf("%d",&a);
+	fflush(stdin);
+	if(a)
+		regist();
+	else
+		login();
+}
+
+int check(int a)
+{
+	switch (a)
+	{
+	case 1:
+		game();
+		return 0;
+		
+	case 0:
+		return 0;
+		
+		default:
+			printf("选项错误!\n请重新输入：\n");
+			return 1;
+	}
 }
 
 int main()
 { 
 	int a;
-    meun();
-    printf("1 Play\n");
-    printf("0 Exit\n");
-    scanf("%d", &a);
-    switch (a)
-	{
-        case 1:
-        game();
-        break;
-        
-        case 0:
-        break;
-        
-        default:
-        printf("选项错误!\n请重新输入：\n");
-    }
-    return 0;
+	meun();
+	printf("1 Play\n");
+	printf("0 Exit\n");
+	while(scanf("%d",&a)&&check(a)){}
+	return 0;
 }
-
